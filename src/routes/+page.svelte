06575da4ff8141425b3f0a131d8ba08b2ae5c1e2
apriego87 <antonio.priego@m.e-tecnia.es>
2025-02-03 13:49:13 +0100

    X pa borrar searchbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 	// Debounce variables
 	let debounceTimeout: any;
 
-	async function fetchStations() {
+	async function fetch_stations() {
 		try {
 			const res = await fetch(
 				'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres'
@@ -45,15 +45,14 @@
 	}
 
 	// Run fetchStations() on mount
-	fetchStations();
+	fetch_stations();
 
 	let has_input = false;
 
-	let test: any[] = []; // To hold the filtered results
+	let filtered_stations: any[] = []; // To hold the filtered results
 
 	// Function to search and store results in the 'test' variable with debounce
-	// Function to search and store results in the 'test' variable with debounce
-	function search_animal() {
+	function search_station() {
 		// Clear the previous debounce timeout to reset the delay
 		clearTimeout(debounceTimeout);
 
@@ -65,7 +64,7 @@
 		// Set a new timeout to execute the filtering after the user stops typing
 		debounceTimeout = setTimeout(() => {
 			// Clear out previous search results
-			test = [];
+			filtered_stations = [];
 
 			// If the input is not empty, filter the stations
 			if (searchTerms.length > 0) {
@@ -76,15 +75,23 @@
 
 					// Check if all search terms are included in searchProps
 					if (searchTerms.every((term) => obj.searchProps.includes(term))) {
-						test = [...test, obj]; // Adding to test (reactively)
+						filtered_stations = [...filtered_stations, obj]; // Adding to test (reactively)
 					}
 				}
 			} else {
 				// If the search is empty, reset to the full stations array and set has_input to false
-				test = stations;
+				filtered_stations = stations;
 				has_input = false; // Set has_input to false when search bar is empty
 			}
 		}, 300); // 300 milliseconds delay
+	}
+
+	// Function to clear the search bar and reset results
+	function clear_search() {
+		const searchInput = document.getElementById('searchbar') as HTMLInputElement;
+		searchInput.value = ''; // Clear the input field
+		filtered_stations = []; // Clear filtered stations
+		has_input = false; // Set has_input to false
 	}
 
 	function capitalize(text: string) {
@@ -111,21 +118,44 @@
 	<p>No stations found.</p>
 {:else}
 	<div class="sticky top-0 isolate z-10 my-5 flex w-full flex-row justify-center gap-4">
-		<input
-			id="searchbar"
-			on:input={search_animal}
-			type="text"
-			name="search"
-			placeholder="Busca por nombre..."
-			class="rounded border p-2"
-		/>
+		<div class="relative">
+			<input
+				id="searchbar"
+				on:input={search_station}
+				type="text"
+				name="search"
+				placeholder="Busca por nombre..."
+				class="rounded border p-2"
+			/>
+			{#if has_input}
+				<button
+					type="button"
+					on:click={clear_search}
+					class="m-2 text-gray-500 hover:text-gray-800"
+				>
+					<svg
+						width="15"
+						height="15"
+						viewBox="0 0 15 15"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z"
+							fill="currentColor"
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+						></path></svg
+					>
+				</button>
+			{/if}
+		</div>
 	</div>
+
 	{#if has_input}
 		{#each provinces as province}
-			{#if test.some((station) => station.Provincia === province)}
-				<!-- Check if there's any station for this province in 'test' -->
+			{#if filtered_stations.some((station) => station.Provincia === province)}
 				<div class="grid w-full max-w-full gap-4 overflow-auto bg-gray-300 p-5 md:grid-cols-3">
-					{#each test.filter((station) => station.Provincia === province) as station}
+					{#each filtered_stations.filter((station) => station.Provincia === province) as station}
 						<Card.Root class="flex flex-col justify-between">
 							<Card.Header>
 								<Card.Title>
@@ -155,7 +185,7 @@
 					<Accordion.Trigger class="mx-2">{province}</Accordion.Trigger>
 					<Accordion.Content>
 						<div class="grid w-full max-w-full gap-4 overflow-auto bg-gray-300 p-5 md:grid-cols-3">
-							{#each (test.length > 0 ? test : stations).filter((station) => station.Provincia === province) as station}
+							{#each (filtered_stations.length > 0 ? filtered_stations : stations).filter((station) => station.Provincia === province) as station}
 								<Card.Root class="flex flex-col justify-between">
 									<Card.Header>
 										<Card.Title>
